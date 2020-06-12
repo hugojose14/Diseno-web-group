2bf6a991c4235f8a49cdddb592b41c22d9dbbdf4
jesucluna <jesucluna@gmail.com>
2020-06-11 23:09:12 -0500

    Updates

---
+++ precache-manifest.bb017b1fc6b1f196dd120551d97b0354.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b3d7e55dcd9f9c4c4f28532642d63d",
+    "url": "/Diseno-web-group/index.html"
+  },
+  {
+    "revision": "d990f6d8b862aabae9a8",
+    "url": "/Diseno-web-group/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "ef75310a996de77a6c83",
+    "url": "/Diseno-web-group/static/css/main.e79ebd9f.chunk.css"
+  },
+  {
+    "revision": "d990f6d8b862aabae9a8",
+    "url": "/Diseno-web-group/static/js/2.34d360d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Diseno-web-group/static/js/2.34d360d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef75310a996de77a6c83",
+    "url": "/Diseno-web-group/static/js/main.f01ab90d.chunk.js"
+  },
+  {
+    "revision": "c5b7e4d1d37d986ed3f2",
+    "url": "/Diseno-web-group/static/js/runtime-main.9f2a1dd1.js"
+  },
+  {
+    "revision": "e2f36b6c6ef96564f853a40475dd0773",
+    "url": "/Diseno-web-group/static/media/gotham.e2f36b6c.otf"
+  }
+]);
